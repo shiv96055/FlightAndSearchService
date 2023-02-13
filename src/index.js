1c0dde6769f918bce793fbd68f20141f00102dec
shiv <shiv96055@gmail.com>
2023-02-13 17:37:58 +0530

    Setup basic bodyParser

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 const express = require("express");
+const bodyParser = require("body-perser");
 
 const { PORT } = require('./config/serverConfig');
 const setupAndStartServer = async () =>{
 
     // crate express object
     const app = express() ;
+
+    // middle ware
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencode({extended : true}));
+
     app.listen(PORT, () => {
         console.log(`Server Start at ${PORT}`);
-        console.log(process.env);
+       // console.log(process.env);
     });
 
 
